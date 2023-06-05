17f65049a4af2d2b2dc235f9e907de8d139ae306
卜木 <xqbumu@gmail.com>
2023-06-05 22:22:28 +0800

    chore: add lang config

--- code/utils/i18n.go
+++ code/utils/i18n.go
@@ -8,3 +8,8 @@ import (
 // 设置全局语言
 var lang language.Tag = language.Vietnamese
 var I18n = message.NewPrinter(lang)
+
+func SetI18n(s string) {
+	lang = language.MustParse(s)
+	I18n = message.NewPrinter(lang)
+}
